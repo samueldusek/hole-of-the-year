1f95b38fd613a265b9bb559d19535328c2a83d63
samueldusek <smldsk@protonmail.com>
2021-06-22 19:09:58 +0200

    add pagination for search queries on /views/courses/index page

--- controllers/courses.js
+++ controllers/courses.js
@@ -6,31 +6,33 @@ const slugify = require("slugify");
 const ITEMS_PER_PAGE = 8;
 
 module.exports.showAllCourses = async (req, res) => {
-  // Check if there is a seach query
-  if (req.query.q) {
-    const slug = slugify(req.query.q, { lower: true });
-    const searchQuery = `/${slug}/i`;
-    const foundCourses = await Course.find({ slug: { $regex: slug } }).populate(
-      "holes"
-    );
+  let courses = [];
+  let numberOfCourses;
+  let slug = '';
+
+  // Fetch all the courses names from db - wil be used latter as a hint in search bar
+  const allCoursesNames = await Course.find({}, { name: 1, _id: 0 });
+
+  // Find out the total number of courses to display across all pages
+  if(req.query.q){
+    slug = slugify(req.query.q, { lower: true });
+    numberOfCourses = await Course.find({ slug: { $regex: slug } }).countDocuments();
     // Check if any course was found and if not tell the user to change his query
-    if (foundCourses.length === 0) {
+    if (!numberOfCourses) {
       req.flash(
         "error",
         `Pro výraz "${req.query.q}" nebylo nalezeno žádné hřiště. Zkus to prosím znovu.`
       );
       return res.redirect(`/courses/`);
     }
+  } else {
+    numberOfCourses = allCoursesNames.length;
   }
-
-  // Fetch all the courses names from db - wil be used latter as a hint in search bar
-  const allCoursesNames = await Course.find({}, { name: 1, _id: 0 });
-  const numberOfCourses = allCoursesNames.length;
 
   // Calculate the last page and check whether the query.page exceeds calculated value
   const lastPage = Math.ceil(numberOfCourses / ITEMS_PER_PAGE);
 
-  // Check if the request is comming from page different from 1
+  // Check if the request is comming from page different from 1st
   let page = req.query.page;
   if (!page || !parseInt(page) || page < 1) {
     page = 1;
@@ -39,11 +41,21 @@ module.exports.showAllCourses = async (req, res) => {
   } else {
     page = Math.floor(page);
   }
-
-  const courses = await Course.find({})
-    .skip((page - 1) * ITEMS_PER_PAGE)
+  
+  // Check if there is a search query and find all courses to display for related page
+  if (req.query.q) {
+    courses = await Course.find({ slug: { $regex: slug } }).skip((page - 1) * ITEMS_PER_PAGE)
     .limit(ITEMS_PER_PAGE)
     .populate("holes");
+  } else {
+  // Fetch all the courses for related page if there is no search query
+    courses = await Course.find({})
+      .skip((page - 1) * ITEMS_PER_PAGE)
+      .limit(ITEMS_PER_PAGE)
+      .populate("holes");
+       numberOfCourses = allCoursesNames.length;
+    } 
+
   res.render("courses/index", {
     allCoursesNames: allCoursesNames,
     courses: courses,
@@ -55,6 +67,7 @@ module.exports.showAllCourses = async (req, res) => {
     previousPage: page - 1,
     currentPage: page,
     lastPage: lastPage,
+    searchQuery: slug.length > 0 ? `&q=${slug}` : slug
   });
 };
 
