d5ae2038f5cad2dd9090952127e782bc2c91e347
samueldusek <smldsk@protonmail.com>
2021-09-21 17:29:17 +0200

    add new seeds files

---
+++ seeds/create-data.js
@@ -0,0 +1,59 @@
+// Require .env file
+require("dotenv").config({ path: "../.env" });
+
+// Load installed packages
+const mongoose = require("mongoose");
+const fs = require("fs");
+
+// Load models
+const Course = require("../models/course");
+
+// Connect to MongoDB
+mongoose.connect(process.env.DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+  console.log("Database connected");
+});
+
+const createData = async () => {
+  const coursesData = [];
+  const courses = await Course.find({}).populate("holes");
+  for (let course of courses) {
+    coursesData.push({
+      name: course.name,
+      slug: course.slug,
+      type: course.type,
+      region: course.region,
+      folderId: course.folderId,
+      image: {
+        url: "link",
+        author: {
+          name: "Author's name",
+          tag: "Author's tag",
+          url: "link",
+        },
+      },
+      holes: course.holes.map((hole) => ({
+        number: hole.number,
+        length: hole.length,
+        par: hole.par,
+        image: {
+          url: "link",
+        },
+      })),
+    });
+  }
+  fs.writeFile("courses.json", JSON.stringify(coursesData), (error) => {
+    if (error) throw error;
+    console.log("The courses were saved to courses.json");
+  });
+};
+
+createData().then(() => {
+  db.close();
+});
